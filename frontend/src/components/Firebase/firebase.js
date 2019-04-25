dc8ba70bb4e34c64b4e4485ab39dd87f071412b5
Bill Jang <malloc099@gmail.com>
2019-04-25 17:06:34 +0900

    Hide env

--- frontend/src/components/Firebase/firebase.js
+++ frontend/src/components/Firebase/firebase.js
@@ -1,30 +1,19 @@
 import app from "firebase/app";
 import "firebase/auth";
 
-require("dotenv");
-
-// const config = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.FIREBASE_DATABASE_URL,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET, 
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-// };
-
 const config = {
-    apiKey: "AIzaSyBsEGPSAlpX3FV8YD-rD1LIBcpcZK_GL-Q",
-    authDomain: "ams-icists-182409.firebaseapp.com",
-    databaseURL: "https://ams-icists-182409.firebaseio.com",
-    projectId: "ams-icists-182409",
-    storageBucket: "ams-icists-182409.appspot.com",
-    messagingSenderId: "650198246298"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
     constructor() {
         app.initializeApp(config);
-
+               
         this.auth = app.auth();
     }
 
