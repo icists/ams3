9ba3454fcb5d6493fcdef5c0c82251048abb99c2
Bill Jang <malloc099@gmail.com>
2019-04-25 09:02:49 +0900

    Dashboard Outline

---
+++ frontend/src/containers/PaymentInformation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class PaymentInformation extends React.Component {
+    constructor() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                PaymentInformation
+            </div>
+        );
+    }
+}
+
+export default PaymentInformation;
---
+++ frontend/src/containers/PersonalInformation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class PersonalInformation extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                PersonalInformation
+            </div>
+        );
+
+    }
+}
+
+export default PersonalInformation;
