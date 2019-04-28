3094c99bf8fd66a30d9d1c279409bb0d950e3a86
TheStarkor <sk_and_mc@kaist.ac.kr>
2019-04-28 14:42:17 +0900

    Deploy

---
+++ frontend/src/components/Event/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Event extends Component {
+  render() {
+    return (
+      <div className="event">
+        <div className="row">
+        <div className="col-1"></div>
+        <div className="col-10">
+        <h2>Open Soon..</h2>
+        </div>
+        <div className="col-1"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Event
