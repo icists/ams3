2bbd642883ebecd943f86790b9bd5601eb14a8a1
TheStarkor <sk_and_mc@kaist.ac.kr>
2019-04-29 03:23:41 +0900

    change poster

--- frontend/src/components/Home/Icists2019/index.js
+++ frontend/src/components/Home/Icists2019/index.js
@@ -13,7 +13,7 @@ export class Icists2019 extends Component {
             <img src={poster} className="poster" alt=""/>
             <h2 className="text-uppercase font-weight-bold">The Art of Science: Expression</h2>
             <img src={topicimg} alt=""/>
-            <h4>[The Sower, Millet(left), van Gogh(right)]</h4>
+            <h3>The Sower, Millet(left), van Gogh(right)</h3>
             <div className="topic">
             <p>Vincent van Gogh is known for having redrawn Millet’s The Sower over 10 times throughout his short lifetime. Even though the same object was composed, he was able to convey a different emotion to the public at large by applying his own unique spin. Similar cases can be found in the field of science. Being expressed mainly through lab experiments and papers, science is usually considered difficult and distant. As van Gogh was able to recreate Millet’s work into something entirely different, depending on how it is expressed, science can be shown as much more approachable and entertaining. ICISTS 2019 aims to discuss the various ways of expressing science in a much more aesthetic and vibrant manner.</p>
             <p>Those who are introduced to science only through complex textbooks are bound to feel some degree of reluctance. To visually represent an abstract image in one’s imagination, a blank canvas is needed. Likewise, a different medium is required to convey professional scientific knowledge to the public. Every author, lecturer, creator and all those who engage in the popularization of science can provide a solution to this matter. Using their own language, these communicators bring about new interest and curiosity from people and contribute to the spreading of science culture.</p>
